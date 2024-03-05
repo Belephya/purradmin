51de482149eee0d0f3a743c4e496427d4acfa494
Belephya <belephya@pandemonium.co.uk>
2024-03-05 11:46:58 +0000

    Added auth

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -7,7 +7,8 @@ import { defineAuth } from '@aws-amplify/backend';
  */
 export const auth = defineAuth({
   loginWith: {
-    email: true,
+    email: {verificationEmailSubject: 'Welcome! verify you email!'
+    },
     // add social providers
     // externalProviders: {
     /**
