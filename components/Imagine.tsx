3e617196a6c58c8e8223e85bca8cf0b005329452
Belephya <belephya@pandemonium.co.uk>
2024-03-06 22:34:32 +0000

    fixed image url

--- components/Imagine.tsx
+++ components/Imagine.tsx
@@ -2,25 +2,31 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { Configuration, OpenAIApi } from 'openai';
+import OpenAI from 'openai';
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 
 
 const ImageGenerator = () => {
     const [query, setQuery] = useState('');
-    const [generatedImage, setGeneratedImage] = useState(null);
+    const [generatedImage, setGeneratedImage] = useState<string | undefined>(undefined);
+    
   
     // Function to call the DALL-E API
     const generateImage = async () => {
-      try {
-        const apiKey = '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'; // Replace with your actual API key
-        const openai = new OpenAIApi(new Configuration({ apiKey }));
-        const response = await openai.createImage({ prompt: query });
-        setGeneratedImage(response.data.url); // Assuming the API response includes an image URL
-      } catch (error) {
-        console.error('Error generating image:', error);
-      }
+        try {
+            const apiKey = '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'; // Replace with your actual API key
+            const openai = new OpenAI({ apiKey });
+            const response = await openai.images.generate({ 
+                    model: "dall-e-3",
+                    prompt: query,
+                    n:1,
+                    size:"512x512",            
+            });
+            setGeneratedImage(response.data[0].url); // Assuming the API response includes an image URL
+        } catch (error) {
+            console.error('Error generating image:', error);
+        }
     };
   
     return (
